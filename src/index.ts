056e7b4c4ee4796aea82019643bf77ea32a6e787
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-06-26 09:00:03 -0500

    feat: add listMedia function to display media items in the CLI

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,20 @@ const cliOptions: CacParsedArgv = clic.parse(process.argv, { run: false });
 
 const [argDir] = cliOptions.args;
 
+function listMedia(db: Database): void {
+  console.log(
+    Object.values(db.map)
+      .map((m) => {
+        const mediaTitle = m.mediaFile.xtitle || m.mediaFile.filename;
+        m.keywords.push("test");
+        return { mediaTitle, keywordString: m.keywords.join() };
+      })
+      .toSorted((a, b) => a.mediaTitle.localeCompare(b.mediaTitle))
+      .map((m) => m.mediaTitle.concat(" > ").concat(m.keywordString))
+      .join("\n"),
+  );
+}
+
 async function main() {
   clack.intro("📸 Influenca - EXIF Metadata Viewer");
 
@@ -56,8 +70,14 @@ async function main() {
       await db.write();
     }
   }
-  await namingWorkflow(db);
-  await db.write();
+
+  if (cliOptions.options.list) {
+    listMedia(db);
+  } else {
+    await namingWorkflow(db);
+    await db.write();
+  }
+
   clack.outro("Done! 🎉");
 }
 
