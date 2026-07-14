499cc4ad6835d065931255c192d868a20b246c83
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-07-14 16:46:23 -0500

    chore: add new video and image fixtures for testing

--- packages/cli/e2e-all.sh
+++ packages/cli/e2e-all.sh
@@ -4,7 +4,7 @@ set -eu
 
 pnpm run build
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
-packages/cli/dist/bin.mjs ascession fixtures/vidz --output "tmp/$TIMESTAMP"
+packages/cli/dist/bin.mjs ascession fixtures/a --output "tmp/$TIMESTAMP"
 packages/cli/dist/bin.mjs analyze "tmp/$TIMESTAMP"
 
 printf "\033[32mHello \033[1;35mWorld\033[0m\n"
