828c1da9d0e9826bf90831fdf9c4e17ea8b16013
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-07-14 18:42:40 -0500

    fix: update build command in e2e-all.sh to use filter for cli package

--- packages/cli/e2e-all.sh
+++ packages/cli/e2e-all.sh
@@ -2,9 +2,12 @@
 
 set -eu
 
-pnpm run build
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+
+pnpm --filter @influenca/cli... run build
+
 packages/cli/dist/bin.mjs ascession fixtures/a --output "tmp/$TIMESTAMP"
+
 packages/cli/dist/bin.mjs analyze "tmp/$TIMESTAMP"
 
 printf "\033[32mHello \033[1;35mWorld\033[0m\n"
