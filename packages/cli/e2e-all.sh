b2f46afb1696243dab5f84c15bdcdfdf63bf1158
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-07-15 07:17:56 -0500

    fix: update output fixture and greeting in e2e-all.sh

--- packages/cli/e2e-all.sh
+++ packages/cli/e2e-all.sh
@@ -6,10 +6,8 @@ TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 
 pnpm --filter @influenca/cli... run build
 
-packages/cli/dist/bin.mjs ascession fixtures/a --output "tmp/$TIMESTAMP"
+packages/cli/dist/bin.mjs ascession fixtures/b --output "tmp/$TIMESTAMP"
 
 packages/cli/dist/bin.mjs analyze "tmp/$TIMESTAMP"
 
-printf "\033[32mHello \033[1;35mWorld\033[0m\n"
-
-
+printf "\033[32mHello \033[1;35mPurple\033[0m\n"
