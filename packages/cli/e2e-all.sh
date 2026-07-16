511fb7cd58b152e5329acffda322bbef4e270661
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-07-16 14:51:10 -0500

    fix: correct output flag and improve e2e-all.sh script

--- packages/cli/e2e-all.sh
+++ packages/cli/e2e-all.sh
@@ -6,8 +6,8 @@ TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 
 pnpm --filter @influenca/cli... run build
 
-packages/cli/dist/bin.mjs ascession fixtures/b --output "tmp/$TIMESTAMP"
+packages/cli/dist/bin.mjs accession fixtures/b --outDir "tmp/$TIMESTAMP" --no-timestamp --verbose
 
-packages/cli/dist/bin.mjs analyze "tmp/$TIMESTAMP"
+# packages/cli/dist/bin.mjs analyze "tmp/$TIMESTAMP"
 
 printf "\033[32mHello \033[1;35mPurple\033[0m\n"
