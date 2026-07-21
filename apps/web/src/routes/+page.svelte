c62a1b9cae98a0d3184bd2cfa0f32d5937325bea
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-07-21 15:01:57 -0500

    Stub video player with captions and styling

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let videoElement: HTMLVideoElement;
+
+  onMount(() => {
+    // Add text track and cues once the video element is mounted and available
+    const track = videoElement.addTextTrack("captions", "Captions", "en");
+    track.mode = "showing";
+
+    track.addCue(new VTTCue(0, 0.9, "Hildy!"));
+    track.addCue(new VTTCue(1, 1.4, "How are you?"));
+    track.addCue(new VTTCue(1.5, 2.9, "Tell me, is the lord of the universe in?"));
+    track.addCue(new VTTCue(3, 4.2, "Yes, he's in - in a bad humor"));
+    track.addCue(new VTTCue(4.3, 6, "Somebody must've stolen the crown jewels"));
+
+    console.log(track.cues);
+  });
+</script>
+
+<div class="container">
+  <!-- Bind the video element using bind:this -->
+  <video bind:this={videoElement} controls src="/corpus/VID00000.mp4">
+    <track kind="captions" />
+  </video>
+</div>
+
+<style>
+  video {
+    width: 420px;
+    height: 300px;
+  }
+</style>
