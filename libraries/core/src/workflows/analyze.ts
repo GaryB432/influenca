630bc9442cb5eab30f2d01c43fd3b3969a3f0ca7
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-07-27 22:39:38 -0500

    by George I think we've got it

--- libraries/core/src/workflows/analyze.ts
+++ libraries/core/src/workflows/analyze.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
+import { greet } from "@influenca/shared";
 import fs from "node:fs";
 import path, { join } from "node:path";
-import { greet } from "@influenca/shared";
 
 import {
   parseManifest,
